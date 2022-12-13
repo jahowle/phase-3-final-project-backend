985541895d47769d730a040fc75ce8734347683f
Jonny <jonny.howle@gmail.com>
2022-12-13 12:29:09 -0700

    added migration code for Neighbors

--- db/migrate/20221212235215_create_neighbors.rb
+++ db/migrate/20221212235215_create_neighbors.rb
@@ -1,4 +1,11 @@
 class CreateNeighbors < ActiveRecord::Migration[6.1]
   def change
+    create_table :neighbors do |t|
+      t.string :name
+      t.string :bio
+      t.integer :location_id
+      t.integer :need_id
+      t.integer :partner_id
+    end
   end
 end
