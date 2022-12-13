fc9285f1829ed84ea57ee6e69c0254d65e3267d1
Jonny <jonny.howle@gmail.com>
2022-12-13 12:30:57 -0700

    added migration code for Locations

--- db/migrate/20221212235227_create_locations.rb
+++ db/migrate/20221212235227_create_locations.rb
@@ -1,4 +1,9 @@
 class CreateLocations < ActiveRecord::Migration[6.1]
   def change
+    create_table :locations do |t|
+      t.string :name
+      t.integer :neighbor_id
+      t.integer :partner_id
+    end
   end
 end
