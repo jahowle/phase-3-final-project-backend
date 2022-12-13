f1c199117164a40087b7123e049b89cc7e05c65f
Jonny <jonny.howle@gmail.com>
2022-12-13 12:21:56 -0700

    added migration code for Needs table

--- db/migrate/20221212235202_create_needs.rb
+++ db/migrate/20221212235202_create_needs.rb
@@ -1,4 +1,10 @@
 class CreateNeeds < ActiveRecord::Migration[6.1]
   def change
+    create_table :needs do |t|
+      t.string :description
+      t.integer :amount
+      t.integer :neighbor_id
+      t.string :category
+    end
   end
 end
