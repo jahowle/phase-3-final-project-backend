59f6145a5f207c9374df31e33baec8eabec56e09
Jonny <jonny.howle@gmail.com>
2022-12-13 12:32:54 -0700

    added migration code for Partners table

--- db/migrate/20221212235237_create_partners.rb
+++ db/migrate/20221212235237_create_partners.rb
@@ -1,4 +1,9 @@
 class CreatePartners < ActiveRecord::Migration[6.1]
   def change
+    create_table :partners do |t|
+      t.string :name
+      t.integer :location_id
+      t.integer :neighbor_id
+    end
   end
 end
