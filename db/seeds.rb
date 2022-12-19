f6a29a4d569e05315cd7753c1f424ace32559ae3
Jonny <jonny.howle@gmail.com>
2022-12-19 11:22:12 -0600

    added destroy to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 puts "🌱 Seeding spices..."
 
 
-
+Neighbor.destroy_all
 
 
 10.times do
 
     Neighbor.create(
-        name: Faker::Name.name
-        bio: Faker::Lorem.paragraph
-        location_id: Location.pluck(:id).sample
+        name: Faker::Name.name,
+        bio: Faker::Lorem.paragraph,
+        location_id: Location.pluck(:id).sample,
         partner_id: Partner.pluck(:id).sample
         )
 
